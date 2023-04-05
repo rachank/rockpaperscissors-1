70a0e14c0e771fa270588689b2969c49df0eafb1
rachank <rhanka@protonmail.com>
2023-04-05 14:30:58 +0000

    game logic notes in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,40 @@
 
 
 
+/* 
+1. user comes to page and enters player name
+1a. player name is placed into score board
+1b. player name is also entered into winner/loser annoucement
+
+2. user clicks on one of the images of rock, paper, scissors
+
+3. when user clicks on image, computer is prompted to also make
+random image choice in background
+
+4. number of games played is counted and recorded
+
+5. program determines who wins the game
+
+
+6. program displays the winner
+    - for each character there are 4 possible happy images (win) or 
+        4 sad images (lose).
+    - images are displayed: player first, computer second for winner/loser
+    - there is a random choice of images for happy or sad for each character 
+    - First display winner and loser
+    - Second add in random choice of images
+
+7. Add counter of number of games to 10
+
+8. once 10 games is played, 
+    -add play again button, or 
+    - No play again button and thankyou popup.
+*/
+
+
+
+
+
 
 
 
