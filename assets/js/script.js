bef68a027fe2a8f6985816be6d6865c3f8dbcf21
rachank <rhanka@protonmail.com>
2023-04-16 23:42:06 +0000

    add startImage const query and playGame function and eventListener loop for each image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,17 @@
 
+/**List all constants for DOM elements and game options */
+
+const startImage = document.querySelectorAll(".startimage");
 
 
+/* GameBoard area - play game*/
 
-/* 
-
-1a. player name is placed into score board
-1b. player name is also entered into winner/loser annoucement
-
-2. user clicks on one of the images of rock, paper, scissors
-
-3. when user clicks on image, computer is prompted to also make
-random image choice in background
-
-4. number of games played is counted and recorded
-
-5. program determines who wins the game
-
-
-6. program displays the winner
-    - for each character there are 4 possible happy images (win) or 
-        4 sad images (lose).
-    - images are displayed: player first, computer second for winner/loser
-    - there is a random choice of images for happy or sad for each character 
-    - First display winner and loser
-    - Second add in random choice of images
-
-7. Add counter of number of games to 10
-
-8. once 10 games are played, 
-    -add play again button, or 
-    - No play again button and thankyou popup.
-*/
-
-
-
-/* List all constants for DOM elements and choices for game*/
-
-/*var playerName = document.getElementById("playername");*/
-
-function showPlayerName(){
-document.getElementById(displayplayername).innerHTML = document.getElementById("playername").value;
+function playGame(e) {
+  console.log(e.target.id);
 }
 
-
-/*1. user comes to page and enters player name - 
-this name is then placed into the score board*/
-
-
-
-
+// Event Listener for initial click to play game
+startImage.forEach(startimage => startimage.addEventListener('click', playGame));
 
 
 
