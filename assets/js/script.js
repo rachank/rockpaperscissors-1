67c942374a411db89254a7fa630418f867f3aeb5
rachank <rhanka@protonmail.com>
2023-04-17 03:01:50 +0000

    add computer random choice function js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 
 let startImages = document.querySelectorAll(".startimage");
 
+let computer;
 
 /* GameBoard area - play game*/
 
@@ -18,12 +19,22 @@ startImage.forEach(startimage => startimage.addEventListener('click', playGame))
 
 //Computer random choice of images game function
   function getCompPick() {
-
-
+    const randPick = Math.floor(Math.random() * 3) + 1;
+    switch(randPick){
+        case 1:
+            computer = "rock"
+            break;
+        case 2:
+            computer = "paper"
+            break;
+        case 3:
+            computer = "scissors"
+            break;
+    }
   }
+  console.log(compPick);
   
   
-  Math.floor(Math.random() * 3);
 
 
 
